d92f8a57288d8d691daa1e81b446defb174398a5
MeowServer~ <2679977872@qq.com>
2025-12-11 08:55:58 -0500

    Update IllegalTagRemovalMiddleware.cs

--- TextChatMeow/Middlewares/IllegalTagRemovalMiddleware.cs
+++ TextChatMeow/Middlewares/IllegalTagRemovalMiddleware.cs
@@ -1,1 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using TextChatMeow.Core.Interface;
+using TextChatMeow.Core.Models;
+
+namespace TextChatMeow.Middlewares
+{
+    public class IllegalTagRemovalMiddleware : IMiddleware
+    {
+        public int Priority { get; } = 1;
+
+        private static readonly Regex RichTextTagRegex = new Regex(
         @"<(?:\/?[a-zA-Z0-9]+(?:=[^>]*)?)>",
+        RegexOptions.Compiled);
+
+        public void Process(ChatContext chatContext)
+        {
+            if (string.IsNullOrEmpty(chatContext.Message.DisplayContent))
+                return;
+
+            chatContext.Message.DisplayContent = RichTextTagRegex.Replace(chatContext.Message.DisplayContent, string.Empty);
+
+            return;
+        }
+    }
+}
