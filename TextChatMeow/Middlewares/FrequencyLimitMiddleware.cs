d8662d4cc5f5dfb78ce9b3bb7a52f0c8811a787e
MeowServer~ <2679977872@qq.com>
2025-12-11 08:55:32 -0500

    Add Middlewares

---
+++ TextChatMeow/Middlewares/FrequencyLimitMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using TextChatMeow.Core.Interface;
+using TextChatMeow.Core.Models;
+
+namespace TextChatMeow.Middlewares
+{
+    public class FrequencyLimitMiddleware : IMiddleware
+    {
+        public int Priority { get; } = 1;
+
+        private Dictionary<string, List<DateTime>> _userMessageTimestamps = new Dictionary<string, List<DateTime>>();
+        private TimeSpan _timeInterval;
+        private int _maxMessages;
+
+        public FrequencyLimitMiddleware(TimeSpan timeInterval, int maxMessages)
+        {
+            _timeInterval = timeInterval;
+            _maxMessages = maxMessages;
+        }
+
+        public void Process(ChatContext chatContext)
+        {
+            // Skip if sender is not a player
+            if (chatContext.Message.SenderUserId == string.Empty)
+            {
+                return;
+            }
+
+            // Get or create message history for player
+            if (!_userMessageTimestamps.TryGetValue(chatContext.Message.SenderUserId, out var timestamps))
+            {
+                timestamps = new List<DateTime>();
+                _userMessageTimestamps[chatContext.Message.SenderUserId] = timestamps;
+                return;
+            }
+
+            // Remove timestamps outside of time interval
+            foreach (var timestamp in timestamps)
+            {
+                var now = DateTime.UtcNow;
+                if (now - timestamp > _timeInterval)
+                {
+                    timestamps.Remove(timestamp);
+                }
+            }
+
+            // Cancel if message frequency exceeded
+            if (timestamps.Count >= _maxMessages)
+            {
+                chatContext.Cancel("Message frequency limit exceeded.");
+                return;
+            }
+
+            // Add timestamp for current message and continue
+            timestamps.Add(chatContext.Message.TimestampUtc);
+
+            return;
+        }
+    }
+}
---
+++ TextChatMeow/Middlewares/LogMiddleware.cs
@@ -0,0 +1,15 @@
+﻿using LabApi.Features.Console;
+using TextChatMeow.Core.Interface;
+using TextChatMeow.Core.Models;
+
+namespace TextChatMeow.Middlewares
+{
+    public class LogMiddleware : IMiddleware
+    {
+        public int Priority { get; } = int.MaxValue;
+        public void Process(ChatContext chatContext)
+        {
+            Logger.Info($"[ChatLog] {chatContext.Message.SenderDisplayedName}({chatContext.Message.SenderUserId}): {chatContext.Message.RawContent}(Raw Message: {chatContext.Message.RawContent})");
+        }
+    }
+}
